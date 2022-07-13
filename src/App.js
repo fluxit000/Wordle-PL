a8252948e405bf76d32a739a2b8085a4262c482f
Lukasz <lukasztrusk@gmail.com>
2022-07-14 01:03:21 +0200

    Add footer and board

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState } from 'react';
 import './App.css';
+import Github from './icons/github';
 
 function App() {
+  const [board, setBoard] = useState([["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""],["","","","",""]])
+
   return (
-    <div></div>
+    <div id='contener'>
+      <div id='footer'><a target="_blank" href="https://github.com/fluxit000/Wordle-PL"><Github color="var(--dark-font)" size={35}/></a><span id="title">Wordle PL</span></div>
+      <div id="board">
+        {/* <div className='row'><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div></div>
+        <div className='row'><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div></div>
+        <div className='row'><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div></div>
+        <div className='row'><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div></div>
+        <div className='row'><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div></div>
+        <div className='row'><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div><div className='cell'></div></div> */}
+
+        {/* {board.map(row =>
+        <div className='row'>
+          {row.map((item)=><div className='cell'>{item}</div>)}
+        <div/>
+        )} */}
+        
+        {board.map(row=><div className='row'>{row.map(item=><div className='cell'>{item}</div>)}</div>)}
+      </div>
+    </div>
   );
 }
 
