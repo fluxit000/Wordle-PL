a5a67f092156b3c56c5639e30ad1b97d757251c0
fluxit000 <lukasztrusk@gmail.com>
2022-07-16 21:32:33 +0200

    Chnage footer css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import Github from "../icons/github"
+import "./footer.css"
 
 const Footer = ()=>{
     return <div id='footer'>
-        <a target="_blank" href="https://github.com/fluxit000/Wordle-PL">
+        <a className="logo" target="_blank" href="https://github.com/fluxit000/Wordle-PL">
             <Github color="var(--dark-font)" size={35}/>
         </a>
-        <span id="title">Wordle PL</span></div>
-
+        <div id="title">Wordle PL</div>
+    </div>
 }
 
 export default Footer
